f69a55bbfd4b30bc2009f4d313e4388e484d0f05
qxz0kpi <Nuno.Neto@ctw.bmwgroup.com>
2020-10-10 22:04:08 +0100

    New settings

--- addon.py
+++ addon.py
@@ -2,14 +2,12 @@ import subprocess
 import xbmcaddon
 
 my_addon = xbmcaddon.Addon()
-# res = my_addon.getSetting('res')
-# fps = my_addon.getSetting('fps')
-# ip = my_addon.getSetting('ip')
-# bitrate = my_addon.getSetting('bitrate')
+res = my_addon.getSetting('res')
+fps = my_addon.getSetting('fps')
+ipaddress = my_addon.getSetting('ipaddress')
+bitrate = my_addon.getSetting('bitrate')
 
-# moonlight = "moonlight stream -" + res + " -fps " + fps + " -bitrate " + bitrate + "000 " + ip
-
-moonlight = "steamlink"
+moonlight = "moonlight stream -" + res + " -fps " + fps + " -bitrate " + bitrate + "000 " + ipaddress
 
 process = subprocess.Popen(moonlight, stdout=subprocess.PIPE)
 output, error = process = process.communicate()
